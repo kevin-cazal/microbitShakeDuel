ca5d347ad24192d56598a1d94dfeea11aa0de555
kevin-cazal <50819904+kevin-cazal@users.noreply.github.com>
2025-12-17 11:31:05 +0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,46 @@
+/**
+ * # Micro:Bit Shake Duel
+ * 
+ * Rules: Shake the Micro:Bit faster than your opponent to win the duel. The first to win 2 duels wins the match.
+ * 
+ * To win a duel: shake the Micro:Bit 25 time before your opponent does the same.
+ * 
+ * On start:
+ * 
+ * ..Initialize numberOfShake, duelsWon, otherDuelsWon to 0
+ * 
+ * ..Set your radio group number to the same value as your opponent
+ * 
+ * On shake:
+ * 
+ * ..Increase numberOfShake by 1
+ * 
+ * ..If numberOfShake reach 25:
+ * 
+ * ....Increase duelsWon by 1
+ * 
+ * ....Reset numberOfShake to 0
+ * 
+ * ....Send the information to the opponent
+ * 
+ * ....If duelsWon reach 2:
+ * 
+ * ......You win !
+ * 
+ * On radio received number:
+ * 
+ * ..Set this number as a new value for otherDuelsWon
+ * 
+ * ..Reset numberOfShake to 0
+ * 
+ * ..If otherDuelsWon reach 2:
+ * 
+ * ....You lose !
+ */
 radio.onReceivedNumber(function (receivedNumber) {
     otherCompleted = receivedNumber
     music.play(music.stringPlayable("C5 A B G A F G E ", 500), music.PlaybackMode.InBackground)
+    shake = 0
     if (otherCompleted == 2) {
         basic.showIcon(IconNames.Sad)
         basic.pause(2000)
